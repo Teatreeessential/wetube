5e6dacce124cdbee62ce926b05b8f6ede2971e75
ahn <wdefrg1234@naver.com>
2019-02-09 22:37:00 +0900

    웹서버

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+import express from  "express";
+const app = express();
+const PORT = 4000;
+
+const handleListening = () =>{console.log(`Listening on: http://localhost:${PORT}`);}
+
+app.listen(PORT,handleListening);
+
+app.get('/',function(req,res){
+    res.send('hello fuck you');
+})
